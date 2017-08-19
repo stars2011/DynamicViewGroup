cadfdcb68adc626b5b833215fad80a860b9c2105
star <jiaxing.liang@infothinker.com>
2017-08-19 13:09:48 +0800

    create DynamicViewGroup

---
+++ library/src/main/java/com/stars2011/dynamicviewgroup/DynamicViewGroup.java
@@ -0,0 +1,34 @@
+package com.stars2011.dynamicviewgroup;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by stars2011
+ */
+
+public class DynamicViewGroup extends ViewGroup {
+
+    public DynamicViewGroup(Context context) {
+        this(context, null);
+    }
+
+    public DynamicViewGroup(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DynamicViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
